16c0ccf9393496feb6a414fb272d94b4b5fe933f
Joshua <benasjoshuac@gmail.com>
2024-11-01 20:16:19 +0800

    fix minor changes design

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -27,7 +27,7 @@ const Create = ({ addTodo }) => {
   }
 
   return (
-    <div className='mb-4 mx-auto flex flex-col sm:flex-row gap-2 justify-center border border-white'>
+    <div className='mb-4 mx-auto flex flex-col sm:flex-row gap-2 justify-center'>
       <input type="text" placeholder='type your task here' value={task} onChange={(e) => setTask(e.target.value)} className=' w-auto px-4 py-2 rounded-md text-black' required />
       <button type='button' onClick={handleAdd} className='sm:ml-2 text-white uppercase bg-black px-4 py-2 rounded-md hover:bg-gray-900'>add</button>
     </div>
